7c903204f002dcf437739c9806ad6efc89590912
nadirs <nadirsampaoli@gmail.com>
2015-01-22 21:53:51 +0100

    Reorganize package and add some code

---
+++ src/Game/Hearts.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Game.Hearts where
+
+import Prelude hiding (foldr)
+import Data.Monoid (Monoid)
+import Data.Foldable
+import Data.Set (Set)
+import qualified Data.Set as S
+
+import Test.QuickCheck
+
+-- Data types
+data CardSuit = Hearts
+              | Diamonds
+              | Clubs
+              | Spades
+              deriving (Eq, Ord, Enum, Show)
+
+data CardRank = Two
+               | Three
+               | Four
+               | Five
+               | Six
+               | Seven
+               | Eight
+               | Nine
+               | Ten
+               | Jack
+               | Queen
+               | King
+               | Ace
+               deriving (Eq, Ord, Enum, Show)
+
+data Card = Card { cardRank :: CardRank, cardSuit :: CardSuit } deriving (Eq, Ord, Show)
+
+type Score = Int
+
+newtype Deck' a = Deck' { unDeck :: Set a } deriving (Monoid,Foldable)
+type Deck = Set Card
+
+
+-- Instances
+instance Arbitrary CardSuit where
+    arbitrary = elements [Hearts ..]
+
+instance Arbitrary CardRank where
+    arbitrary = elements [Two ..]
+
+instance Arbitrary Card where
+    arbitrary = do
+        r <- arbitrary
+        s <- arbitrary
+        return $ Card r s
+
+instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
+    arbitrary = S.fromList `fmap` arbitrary
+
+
+-- Functions
+{-
+fromList :: [Card] -> Deck
+fromList = Deck' . S.fromList
+
+toList :: Deck -> [Card]
+toList = S.toList . unDeck
+-}
+
+makeDeck :: Deck
+makeDeck = S.fromList [ Card rank suit | suit <- [Hearts ..], rank <- [Two ..]]
+
+cardScore :: Card -> Score
+cardScore (Card _ Hearts) = 1
+cardScore (Card Queen Spades) = 13
+cardScore _ = 0
+
+deckScore :: Deck -> Score
+deckScore deck = if allScoreCards == (allScoreCards `S.intersection` deck) then 0 else foldr (\card acc -> acc + cardScore card) 0 deck
+  where
+    allScoreCards = S.fromList $ [Card rank Hearts | rank <- [Two ..]]
---
+++ tests/HeartsSpec.hs
@@ -0,0 +1,65 @@
+module HeartsSpec (spec) where
+
+-- base modules
+import Prelude hiding (foldr)
+import Data.List (sort, group)
+import Data.Set (toList, fromList)
+import qualified Data.Set as S
+import Data.Foldable (foldr)
+-- testing modules
+import Test.Hspec
+import Test.Hspec.QuickCheck
+-- our modules
+import Game.Hearts
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+spec :: Spec
+spec = do
+    describe "Card" $ do
+        it "can be ordered by rank" $ do
+            Card Ace Hearts `shouldSatisfy` (> Card King Hearts)
+            Card Five Spades `shouldSatisfy` (< Card Jack Spades)
+        context "when counting points" $ do
+            context "when is not a Heart or the Queen of Spades" $ do
+                prop "is worth zero points" $ do
+                    \c -> case c of
+                        Card _ Clubs -> cardScore c == 0
+                        Card _ Diamonds -> cardScore c == 0
+                        _ -> otherwise
+            context "when is a Heart" $ do
+                prop "is worth one point" $ do
+                    \c -> case c of
+                        Card _ Hearts -> cardScore c == 1
+                        _ -> otherwise
+            context "when is the Queen of Spades" $ do
+                prop "is worth 13 points" $ do
+                    \c -> case c of
+                        Card Queen Spades -> cardScore c == 13
+                        _ -> otherwise
+    describe "Deck" $ do
+        it "contains no duplicates" $ do
+            let deck = makeDeck
+            let isUnique [_] = True
+                isUnique _ = False
+                keepDuplicates = filter (not . isUnique)
+            (keepDuplicates . group . sort . toList) deck `shouldBe` []
+        context "as a Foldable instance" $
+            it "can be folded to its card count" $
+                foldr (const succ) 0 makeDeck `shouldBe` (52 :: Int)
+    describe "deckScore" $ do
+        it "counts how many points a deck is worth" $ do
+            let deck = fromList [Card Queen Spades, Card Three Clubs, Card King Hearts, Card Seven Hearts, Card Ace Spades, Card Ten Diamonds]
+            deckScore deck `shouldBe` 15
+            let deck' = fromList [Card Three Hearts, Card Queen Clubs, Card Four Hearts, Card Ace Diamonds, Card Ten Hearts]
+            deckScore deck' `shouldBe` 3
+            let deck'' = fromList [Card Three Clubs, Card Ace Spades, Card Ten Diamonds]
+            deckScore deck'' `shouldBe` 0
+        context "when all Hearts and the Queen of Spades are in Deck" $ do
+            it "a full deck sums up to zero" $ do
+                deckScore makeDeck `shouldBe` 0
+            prop "sums up to zero" $ do
+                let allScoreCards = fromList $ Card Queen Spades : [Card rank Hearts | rank <- [Two ..]]
+                \deck -> if allScoreCards == (allScoreCards `S.intersection` deck)
+                             then deckScore deck == 0
+                             else otherwise
